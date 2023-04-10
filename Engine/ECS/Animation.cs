0334fdbbe9d6c4d411d84d3c8742267cc0a69744
keldon-boehmer <keldon_boehmer@yahoo.com>
2023-04-10 13:58:43 -0600

    Created Animation component class

---
+++ Engine/ECS/Animation.cs
@@ -0,0 +1,23 @@
+﻿
+using Microsoft.Xna.Framework.Graphics;
+using MonoGame.Extended;
+
+namespace BigBlue
+{
+    public class Animation
+    {
+        public float FrameTime { get; set; }
+        public int CurrentFrame { get; set; }
+        public float ElapsedTime { get; set; }
+        public Texture2D[] Frames { get; private set; }
+
+        public Animation(Texture2D[] frames)
+        {
+            Frames = frames;
+            FrameTime = 1000f / (float)frames.Length;
+            CurrentFrame = 0;
+            ElapsedTime = 0f;
+        }
+
+    }
+}
